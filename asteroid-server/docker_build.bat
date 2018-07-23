afb71862a57bb63dd14fd8638beb8732a8989f35
Pierre-Henri <pierreh37@gmail.com>
2018-07-23 22:13:20 +0200

    add docker config for server

---
+++ asteroid-server/docker_build.bat
@@ -0,0 +1,9 @@
+rem install node js
+rem install docker
+
+rem RUN NPM
+call npm install
+call npm run build
+
+rem RUN DOCKER
+docker build -t rphstudio-asteroid-server .
---
+++ asteroid-server/docker_publish.bat
@@ -0,0 +1,3 @@
+docker login
+docker tag rphstudio-asteroid-server:latest registry.admin.rphstudio.net/rphstudio-asteroid-server:latest
+docker push registry.admin.rphstudio.net/rphstudio-asteroid-server:latest
